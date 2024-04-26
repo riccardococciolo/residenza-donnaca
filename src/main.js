1ed46e4085a5f40e725009852d3777fa1dfd361e
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2024-04-26 12:18:08 +0200

    update: build

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router';
+import copy from 'rollup-plugin-copy';
 
 createApp(App).use(router).mount('#app');
+
+export default {
+    plugins: [
+        copy({
+            targets: [
+                { src: 'src/assets/img/*', dest: 'dist/assets' },
+            ],
+        }),
+    ],
+};
